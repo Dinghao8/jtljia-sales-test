9ac374f2e20d0f927105b0814bf12aa54e3dd39c
DingHao <129381945+Dinghao8@users.noreply.github.com>
2024-02-18 22:02:51 +0800

    Update run.py 注释掉allure

--- run.py
+++ run.py
@@ -19,9 +19,9 @@ import shutil
 if __name__ == '__main__':
     pytest.main()
     #复制environment.properties至allure_report目录
-    shutil.copy('./environment.properties', './allure_report')
+    #shutil.copy('./environment.properties', './allure_report')
     #在生成报告前删除已存在的报告文件夹
-    os.system('allure generate ./allure_report -o ./allure_report/html -c ')
+    #os.system('allure generate ./allure_report -o ./allure_report/html -c ')
     #执行allure serve命令打开报告
     #os.system('allure serve ./allure_report/html')
 
